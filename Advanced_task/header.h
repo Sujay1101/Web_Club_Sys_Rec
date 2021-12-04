34523192a2651da1557a7f6c31d80c668ec1ab75
Sujay <58305810+Sujay1101@users.noreply.github.com>
2021-12-04 22:05:17 +0530

    Add files via upload

--- Advanced_task/header.h
+++ Advanced_task/header.h
@@ -1,51 +1,53 @@
-#include <unistd.h>/*contains sbrk system call definition*/
-#include <errno.h>/*Not necessary. Will be helpful to identify type of error encountered if any by sbrk*/
-#include <stdio.h>/*perror defined here*/
-#include <stdbool.h>/*boolean datatype defined here*/
+#include <unistd.h>
+#include <stdbool.h>
 
-//datatype to store metadata of the memory allocation
 typedef struct metadata{
-        size_t size;/*size of memory allocation*/
-        bool occupied;/*True if the memory is in use. False otherwise*/
-} metadata;
+	size_t size;
+	void *next;
+	bool free;
+}metadata;
 
 void *malloc_n(size_t size)
 {
-
-        //Allocate memory to the metadata of the allocation
-        metadata *m = (metadata *)sbrk(sizeof(metadata));
-        
-        //If memory could not be allocated to the metadata print diagnostic message and return NULL
-        if(m == (void *)-1)
-        {
-                perror("");
-                return NULL;
-        }
-        //Initialize size and status of metadata 
-        m->size = size;
-        m->occupied = true;
-
-        //Allocate memory to data
-        void *d = sbrk(size);
-
-        //If memory could not be allocated to the data print diagnostic message and return NULL
-        if(d == (void *)-1)
-        {
-                perror("");
-                return NULL;
-        }
-
-        //Return pointer to data
-        return d;
+	//Scan the free list
+	metadata* b = search(head, size);
+	//If suitable memory block found replace contents of the metadata. 
+	//Set size as original - size and beginning as size bytes more than original beginning  
+	if(b)
+	{
+		occupy(b, size);
+		return (void *)((char *)b + sizeof(metadata));
+	}
+	//If suitable block not found request memory from OS
+	else{
+		m = request_OS(head, size);
+	}
 }
 
-void free_n(void *p)
+metadata *search(metadata *head, size_t size)
 {
-        metadata *m = (metadata *)(p - sizeof(metadata));/*m points to the metadata of memory pointed by p*/
-        m->occupied = false;/*update status of the allocation*/
+	//Traverse the free list
+	metadata *temp = head;
+	if(temp == NULL)
+		return NULL;
+	do
+	{
+		if(temp->size > size)
+			return temp->beginning;
+		temp = temp->next;
 
-	sbrk(-(m->size)); /*deallocate memory occupied by data*/
-	sbrk(-sizeof(metadata));/*deallocate memory occupied by metadata*/
+	}while(temp != head);
 
-        return;
+	return NULL;
+	
 }
+
+void occupy(metadata *key)
+/*
+void replace(metadata *key, ssize_t size)
+{
+	key->beginning = ((char*)key_beginning + size);
+	key->size -= size;
+	return;	
+}*/
+
