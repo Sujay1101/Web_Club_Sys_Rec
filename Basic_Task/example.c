33c22964a31998b5af09a272380c54ebce28b485
Sujay <58305810+Sujay1101@users.noreply.github.com>
2021-12-04 11:07:06 +0530

    Update example.c

--- Basic_Task/example.c
+++ Basic_Task/example.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-#include "malloc_h.h"
+#include <stdio.h>/* definition of printf function*/ 
+#include "malloc_h.h"/* Contains definition of malloc_n function*/
 
 int main()
 {
 	
 	int *a = (int *)malloc_n(sizeof(int) * 4);
 
-	
 	if(a == NULL)
 	{
 		printf("Call to malloc failed\n");
 	}
 
-	
 	return 0;
 }
