d8f565cd61f68acecac6b11efb2416e4429fde02
Sujay <58305810+Sujay1101@users.noreply.github.com>
2021-12-04 11:05:36 +0530

    Update example.c

--- Advanced_task/example.c
+++ Advanced_task/example.c
@@ -1,8 +1,18 @@
-#include "header.h"
+#include "header.h"/* header file contains definietion of malloc_n and free_n functions*/
 #include <stdio.h>
+
 int main()
 {
 	int *p = (int *)malloc_n(sizeof(int) * 4);
+	
+	//If call to malloc failed print diagnostic message and terminate abnormally
+	if(p == NULL)
+	{
+		printf("Call to malloc failed\n");
+		return 1;
+	}
+	
 	free_n(p);
+	
 	return 0;
 }
